04f674ebcf54dcd1d6c5c224e99a275cbdff4d98
Furkan Güleç <2021507115@ogr.oku.edu.tr>
2021-01-16 20:56:42 +0300

    16 Ocak Ders Sonu- Furkan Güleç

---
+++ Metotlar/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //fonksiyonlar da class şeklinde yapılır mesela sepete ekle butonu birden fazla yerde kullanılır o da class seklindedir
+        //naming convention - Fonksiyonlar da büyük harfle başlar
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi ; " + urun.Adi);
+
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi ; " + urunAdi);
+        }
+    }
+}
